8b87f64f220e02bb403f33e59dd7236aedc4a3cc
julianabetancurb <julianabetancur320@gmail.com>
2026-05-26 10:21:29 -0500

    add README and refactor project to remove AWS deployment

--- api.py
+++ api.py
@@ -3,20 +3,23 @@ from pydantic import BaseModel
 from src.dictionary import Dictionary
 from src.costs import get_total
 from src.concatenate import concatenate
-from mangum import Mangum
-app = FastAPI(title="Python Katas API", root_path="/prod")
+
+app = FastAPI(title="Python Katas API")
 
 
 dict_store = Dictionary()
+
 
 class DictEntry(BaseModel):
     word: str
     definition: str
 
+
 @app.post("/dictionary/new-entry")
 def add_entry(entry: DictEntry):
     dict_store.newentry(entry.word, entry.definition)
     return {"message": f"Added word '{entry.word}' successfully."}
+
 
 @app.get("/dictionary/look/{word}")
 def look_entry(word: str):
@@ -28,6 +31,7 @@ class ShoppingCart(BaseModel):
     items: list
     tax: float
 
+
 @app.post("/shopping/total")
 def calculate_total(cart: ShoppingCart):
     total = get_total(cart.costs, cart.items, cart.tax)
@@ -37,8 +41,7 @@ def calculate_total(cart: ShoppingCart):
 class WordList(BaseModel):
     words: list
 
+
 @app.post("/words/concatenate")
 def get_concatenate(data: WordList):
     return {"result": concatenate(data.words)}
-
-handler = Mangum(app, lifespan="off")
